0d31e87a3c234a49a686fb2639b052d5db63ff51
justjonathan-APCS <54410985+justjonathan-APCS@users.noreply.github.com>
2020-02-24 09:14:12 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,34 +5,25 @@ from flask import flash
 import pprint
 import os
 
-# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
-# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
-# environment variables, so that this will work on Heroku.
-# Edited by S. Adams for Designing Software for the Web to add comments and remove flash messaging
-
+#
 app = Flask(__name__)
 
-app.debug = True #Change this to False for production
-
-app.secret_key = os.environ['SECRET_KEY'] #use SECRET_KEY to sign session cookies
+app.debug = True 
+app.secret_key = os.environ['SECRET_KEY']
 oauth = OAuth(app)
 
-# Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web apps "username" for OAuth
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'], # web apps "password" for OAUTh
-    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], 
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'], 
+    request_token_params={'scope': 'user:email'},
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
     access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
-)
+    authorize_url='https://github.com/login/oauth/authorize'
 
-#context  processors run before templates are rendered and add variables to the template context
-#context processors must return a dictionary
-#this context processor adds the variable logged_in to the context for all temlates
+
 @app.context_processor
 def inject_logged_in():
     return {"logged_in":('github_token' in session)}
@@ -40,7 +31,7 @@ def inject_logged_in():
 @app.route('/')
 def home():
     return render_template('home.html')
-#redirect to GITHUB's OAUTH page and confirm the callback URL
+
 @app.route('/login')
 def login():   
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))                                                                                                                   
@@ -50,7 +41,7 @@ def logout():
     session.clear()
     return render_template('message.html', message='You were logged out')
 
-@app.route('/login/authorized')#the route should match the callback URL registered with the OAuth provider
+@app.route('/login/authorized')
 def authorized():
     resp = github.authorized_response()
     if resp is None:
@@ -58,12 +49,12 @@ def authorized():
         flash('Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'])     
     else:
         try:
-            #save user data and set log in message
+            
             session['github_token']=(resp['access_token'],'')
             session['user_data']=github.get('user').data
             flash('You were successfully logged in as ' +  session['user_data']['login'])  
         except:
-            #clear the session and give error message
+           
             session.clear()
             flash('Unable to login. Please try again.')
     return render_template('home.html')
@@ -72,7 +63,7 @@ def authorized():
 @app.route('/page1')
 def renderPage1():
     if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data'])#format the user data nicely
+        user_data_pprint = pprint.pformat(session['user_data']
     else:
         user_data_pprint = '';
     return render_template('page1.html',dump_user_data=user_data_pprint)
@@ -85,7 +76,7 @@ def renderPage2():
             countDRACULA = session['user_data']['public_repos']
     return render_template('page2.html', publicrepocount = countDRACULA)
 
-# the tokengetter is automaticallly called to check who is logged in
+                                          
 @github.tokengetter
 def get_github_oauth_token():
     return session.get('github_token')
@@ -94,10 +85,5 @@ def get_github_oauth_token():
 if __name__ == '__main__':
     app.run()
     
-    #A
-    #G
-    #L
-    #E
-    #T
-    #AGLET
-    #DONT FORGET IT
+   
+    
